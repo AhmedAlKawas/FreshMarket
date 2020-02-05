312c2222887086e9606cd193cffa0c056c0a0e3b
AhmedAlKawas <ahmed.alkawas77@gmail.com>
2020-02-05 23:11:57 +0200

    Implenenting splash screen and adding on boarding activity

--- app/src/main/java/com/example/freshmarket/view/splash_screen/SplashActivity.java
+++ app/src/main/java/com/example/freshmarket/view/splash_screen/SplashActivity.java
@@ -1,12 +1,15 @@
 package com.example.freshmarket.view.splash_screen;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import com.example.freshmarket.R;
 import com.example.freshmarket.databinding.ActivitySplashBinding;
+import com.example.freshmarket.view.onboarding.OnBoardingActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -15,5 +18,22 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ActivitySplashBinding splashBinding = DataBindingUtil.setContentView(this,
                 R.layout.activity_splash);
+        splashScreen();
     }
+
+    private void splashScreen() {
+        Handler handler = new Handler();
+        Runnable r = new Runnable() {
+            public void run() {
+                goToOnBoarding();
+            }
+        };
+        handler.postDelayed(r, 2000);
+    }
+
+    private void goToOnBoarding() {
+        startActivity(new Intent(SplashActivity.this, OnBoardingActivity.class));
+        finish();
+    }
+
 }
